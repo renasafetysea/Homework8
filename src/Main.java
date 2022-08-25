696a75c94f6ffcdbac3bf3068ec537c30efd9063
Anna Molodtsova <renasafetysea@mail.ru>
2022-08-25 18:23:39 +0300

    сделала ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+import java.time.LocalDate;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int year = LocalDate.now().getYear();
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " является високосным");
+        } else {
+            System.out.println(year + " не является високосным");
+        }
+        int clientOS1 = 0;
+        int clientDeviceYear;
+        clientDeviceYear = LocalDate.now().getYear();
+
+        if (clientOS1 == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Утсановите облегченную версию приложения для IOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для IOS по ссылке");
+
+            }
+        } else {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Утсановите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+        int deliveryDistanse = 95;
+
+        int deliveryDays = 1;
+
+        if (deliveryDistanse > 20) {
+            deliveryDays++;
+        }
+        if (deliveryDistanse > 60) {
+            deliveryDays++;
+        }
+        System.out.println("Потребуется дней " + deliveryDays);
+
     }
 }
+
